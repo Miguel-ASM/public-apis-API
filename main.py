b67eb7b06891db991ab6bea4ed043f3592c0664b
Miguel-ASM <miguel.a.s.martinez@gmail.com>
2023-09-18 08:15:31 +0200

    remove unused file

--- main.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-PUBLIC_APIS_README_URI = 'https://raw.githubusercontent.com/public-apis/public-apis/master/README.md'
-
-response = requests.get(PUBLIC_APIS_README_URI)
-
-print( response.text )
