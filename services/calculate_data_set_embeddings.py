fe0bf0f72cd11ef04b04566ea3b799ff24e2e406
Miguel-ASM <miguel.a.s.martinez@gmail.com>
2023-09-24 18:20:36 +0000

    calculate dataset embeddings using openai

--- services/calculate_data_set_embeddings.py
+++ services/calculate_data_set_embeddings.py
@@ -1,21 +1,31 @@
 import  json
-from sentence_transformers import SentenceTransformer
 from pathlib import Path
+import openai
 
+openai.api_key = "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"
 
 data_dir = Path(__file__).parent.parent.joinpath('data')
 input_file_path = data_dir.joinpath('apis.json')
 output_file_path = data_dir.joinpath('apis-with-embeddings.json')
 
-model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
 with open(input_file_path) as f:
   apis_list = json.load(f)
 
-embeddings = model.encode([api.get('description') for api in apis_list])
+texts = [ data.get('description') for data in apis_list ]
+
+embeddings_response = openai.Embedding.create(
+  model="text-embedding-ada-002",
+  input=texts
+)
+
+embeddings_iter = (
+    item.get('embedding')
+    for item in embeddings_response.get('data',[])
+)
 
 apis_list_with_embeddings = [
-  api | {'embedding':list(embedding.astype(float))}
-  for api, embedding in zip(apis_list,embeddings)
+  api | {'embedding': embedding }
+  for api, embedding in zip(apis_list,embeddings_iter)
 ]
 
 with open(output_file_path,'w') as f:
