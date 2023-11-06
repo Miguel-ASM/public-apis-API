7e216bea3c0dc0b97178fb110684f0b82c7648d4
Miguel-ASM <miguel.a.s.martinez@gmail.com>
2023-11-06 19:23:52 +0000

    add commits table

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from db import db
 from pgvector.sqlalchemy import Vector
+from datetime import datetime
 
 class Api(db.Model):
   __tablename__ = 'apis'
@@ -12,3 +13,10 @@ class Api(db.Model):
   https = db.Column(db.String())
   auth = db.Column(db.String())
   embedding = db.Column(Vector(1536))
+
+class ApisRepoCommit(db.Model):
+  __tablename__ = 'apis_repo_commits'
+  id = db.Column(db.Integer, primary_key=True)
+  sha = db.Column(db.String(), nullable=False )
+  created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+  updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
